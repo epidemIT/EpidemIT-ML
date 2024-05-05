d2b6dd6292c3c99713fb528b523e73454d6a7159
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2024-05-05 19:27:48 +0700

    Update routes.tsx

--- my-app/src/app/api/generate-voice/routes.tsx
+++ my-app/src/app/api/generate-voice/routes.tsx
@@ -1,10 +1,6 @@
 export async function POST(request: Request) {
   const requestBody = await request.json();
 
-  if (!requestBody.modelUrl) {
-    throw new Error("Missing 'model url' field in the request body");
-  }
-
   if (!requestBody.input) {
     throw new Error("Missing 'input' field in the request body");
   }
@@ -13,17 +9,19 @@ export async function POST(request: Request) {
     throw new Error("Missing 'Hugging Face Access Token'");
   }
 
-  const modelUrl = requestBody.modelUrl;
   const input = requestBody.input;
 
-  const response = await fetch(modelUrl, {
-    headers: {
-      Authorization: `Bearer ${process.env.HUGGING_FACE_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({ inputs: input }),
-  });
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HUGGING_FACE_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ inputs: input }),
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Request Failed");
